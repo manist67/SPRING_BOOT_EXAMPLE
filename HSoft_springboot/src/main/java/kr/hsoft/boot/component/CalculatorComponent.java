ee58f80f01c127923b0e4141769bdc0bf367bf6b
김민성 <manist67@naver.com>
2019-07-31 00:03:02 +0900

    Add a method boilerplate

--- HSoft_springboot/src/main/java/kr/hsoft/boot/component/CalculatorComponent.java
+++ HSoft_springboot/src/main/java/kr/hsoft/boot/component/CalculatorComponent.java
@@ -3,8 +3,13 @@ package kr.hsoft.boot.component;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CalculatorComponent{
+public class CalculatorComponent {
 	public float add(float op1, float op2) {
 		return op1 + op2;
+	}
+
+	public float sub(float op1, float op2) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
