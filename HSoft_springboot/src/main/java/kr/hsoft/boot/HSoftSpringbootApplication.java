e33b8902c45f885b3a84174e29bccd2cb0d16fef
김민성 <manist67@naver.com>
2019-07-31 00:04:06 +0900

    Remove a unused import syntax

--- HSoft_springboot/src/main/java/kr/hsoft/boot/HSoftSpringbootApplication.java
+++ HSoft_springboot/src/main/java/kr/hsoft/boot/HSoftSpringbootApplication.java
@@ -1,6 +1,5 @@
 package kr.hsoft.boot;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
