313ce041c69b3fdc630fdd9b4335df513a86aa1f
Yang Si Yeon <mn04098@naver.com>
2019-08-23 02:59:47 +0900

    카테고리 받아라

---
+++ HSoft_springboot/src/main/java/kr/hsoft/boot/dto/CategoryDTO.java
@@ -0,0 +1,19 @@
+package kr.hsoft.boot.dto;
+
+public class CategoryDTO {
+	private int idx;
+	private String name;
+	
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	public int getIdx() {
+		return this.idx;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return this.name;
+	}
+}
