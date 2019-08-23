539662d8582baaffaa9d4eaac905e6605aae4689
hyeon soo kim <manist67@naver.com>
2019-08-23 09:20:38 +0900

    SHA256

--- HSoft_springboot/src/main/java/kr/hsoft/boot/utils/SHA256.java
+++ HSoft_springboot/src/main/java/kr/hsoft/boot/utils/SHA256.java
@@ -1,8 +1,34 @@
 package kr.hsoft.boot.utils;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class SHA256 {
 	static public String sha256(String origin) {
-		//TODO: sha256 algorithm in here
-		return origin+"hash";
+		String result = "";
+		
+        byte[] a = origin.getBytes();
+        byte[] bytes = new byte[a.length];
+         
+        System.arraycopy(a, 0, bytes, 0, a.length);
+         
+        try {
+            // 암호화 방식 지정 메소드
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(bytes);
+             
+            byte[] byteData = md.digest();
+             
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < byteData.length; i++) {
+                sb.append(Integer.toString((byteData[i] & 0xFF) + 256, 16).substring(1));
+            }
+             
+            result = sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+         
+		return result;
 	}
 }
