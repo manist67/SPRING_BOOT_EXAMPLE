8d4e350b89c938ea53c4ee89ba24b5db9a30863a
Ubuntu <ubuntu@ip-172-31-25-117.ap-northeast-2.compute.internal>
2019-11-14 11:41:13 +0000

    war배포 설정

--- HSoft_springboot/src/main/java/kr/hsoft/boot/HSoftSpringbootApplication.java
+++ HSoft_springboot/src/main/java/kr/hsoft/boot/HSoftSpringbootApplication.java
@@ -1,10 +1,11 @@
 package kr.hsoft.boot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HSoftSpringbootApplication {
+public class HSoftSpringbootApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HSoftSpringbootApplication.class, args);
